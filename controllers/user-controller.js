0f6fd76066af6554a7d5ba2bd3d3812fe8a9d6e1
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-06-15 20:56:24 -0600

    users

---
+++ controllers/user-controller.js
@@ -0,0 +1,67 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+
+    // GET /api/users
+    getAllUsers(req, res) {
+        User.find({})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    // GET /api/users/:id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate([
+            { path: 'thoughts', select: "-__v" },
+            { path: 'friends', select: "-__v" }
+        ])
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // POST /api/users
+    // expected body:
+    // {
+    //     "username": "foo",
+    //     "email": "bar@baz.com"  // must follow the email format
+    // }
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // PUT /api/users/:id
+    // expected body includes at least one of the attributes below:
+    // {
+    //     "username": "foo",
+    //     "email": "bar@baz.com"  // must follow the email format
+    // }
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+   
