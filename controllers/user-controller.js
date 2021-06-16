969e669ae5f3d94eb1826405f67be96bbe8d3d6a
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-06-15 21:51:52 -0600

    users done?

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -121,3 +121,37 @@ const userController = {
         })
         .catch(err => res.json(err));
     },
+
+    // DELETE /api/users/:userId/friends/:friendId
+    deleteFriend({ params }, res) {
+        // remove friendId from userId's friend list
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this userId' });
+                return;
+            }
+            // remove userId from friendId's friend list
+            User.findOneAndUpdate(
+                { _id: params.friendId },
+                { $pull: { friends: params.userId } },
+                { new: true, runValidators: true }
+            )
+            .then(dbUserData2 => {
+                if(!dbUserData2) {
+                    res.status(404).json({ message: 'No user found with this friendId' })
+                    return;
+                }
+                res.json({message: 'Successfully deleted the friend'});
+            })
+            .catch(err => res.json(err));
+        })
+        .catch(err => res.json(err));
+    }
+}
+
+module.exports = userController;
